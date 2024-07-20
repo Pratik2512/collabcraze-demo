586719a0dce12e2e48322a94b7672131153cf73c
Pratik Mohite <79019830+Pratik2512@users.noreply.github.com>
2024-07-20 18:00:01 +0530

    Add files via upload

---
+++ first_program.c
@@ -0,0 +1,10 @@
+// First program from Sem 1 of BCA Science Course
+
+#include <stdio.h>
+
+int main()
+{
+    char wish[]= "Hello World!";
+    printf("The first progam always starts with customary '%s'", wish);
+    return 0;
+}
